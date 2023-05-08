6f396ef9a41bbb997ae87824f078ad7bcf1be799
victoriasyluma <victoriasyluma3@gmail.com>
2023-05-08 22:44:49 +0200

    finished logic plus styles

--- src/components/logOutButton/LogOut.jsx
+++ src/components/logOutButton/LogOut.jsx
@@ -5,7 +5,6 @@ import styles from './LogOut.module.scss';
 import { logoutThunk } from '../../redux/Auth/authOperations';
 
 export const LogOut = () => {
-  debugger;
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsOnline);
 
@@ -13,14 +12,14 @@ export const LogOut = () => {
     dispatch(logoutThunk());
   };
 
-  if (isLoggedIn) {
-    // TODO: ADD NEGATION
+  if (!isLoggedIn) {
     return null;
   }
 
   return (
     <>
       <button className={styles.logout_button} onClick={handleLogout}>
+        Log out
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="18"
@@ -35,7 +34,6 @@ export const LogOut = () => {
             d="M12.333 13.167 16.5 9m0 0-4.167-4.167M16.5 9h-10m0-7.5h-1c-1.4 0-2.1 0-2.635.272a2.5 2.5 0 0 0-1.093 1.093C1.5 3.4 1.5 4.1 1.5 5.5v7c0 1.4 0 2.1.272 2.635a2.5 2.5 0 0 0 1.093 1.092C3.4 16.5 4.1 16.5 5.5 16.5h1"
           />
         </svg>
-        Log out
       </button>
     </>
   );
