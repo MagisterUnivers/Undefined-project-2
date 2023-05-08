f32f3610262632f97faa0676b1202fbb44d89dac
victoriasyluma <victoriasyluma3@gmail.com>
2023-05-08 22:15:32 +0200

    fisrt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 // import { RegisterForm } from './Forms/RegisterForm/RegisterForm';
 import { LoginForm } from './Forms/LoginForm/LoginForm';
+import { LogOut } from '../components/logOutButton/LogOut';
 
 export const App = () => {
   return (
     <div>
       <LoginForm />
+      <LogOut />
     </div>
   );
 };
