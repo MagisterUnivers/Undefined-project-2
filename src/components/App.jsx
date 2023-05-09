da99e566ab44d64233e3a87ca8505d90d1955ed8
Irgey <116591589+Irgey@users.noreply.github.com>
2023-05-09 20:33:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,22 +8,23 @@ import AccountPage from 'pages/AccountPage/AccountPage';
 import { Header } from './Header/Header';
 
 export const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Routes>
+        <Route path="/about" /> {/*TODO: About page element*/}
+        <Route path="/register" element={<RegisterPage />} />{' '}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/test" element={<UserForm />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/calendar" /> {/*TODO: Calendar page element*/}
+          <Route path="/calendar/day/:currentDay" />{' '}
+          {/*TODO: Calendar day page element*/}
+          <Route path="/calendar/month/:currentDate" />{' '}
+          {/*TODO: Calendar month page element*/}
+        </Route>
+        <Route path="*" element={<div>Page not found...</div>} />
+      </Routes>
+    </>
+  );
 };
-<>
-  <Routes>
-    <Route path="/about" /> {/*TODO: About page element*/}
-    <Route path="/register" element={<RegisterPage />} />{' '}
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/test" element={<UserForm />} />
-    <Route path="/" element={<MainLayout />}>
-      <Route path="/account" element={<AccountPage />} />
-      <Route path="/calendar" /> {/*TODO: Calendar page element*/}
-      <Route path="/calendar/day/:currentDay" />{' '}
-      {/*TODO: Calendar day page element*/}
-      <Route path="/calendar/month/:currentDate" />{' '}
-      {/*TODO: Calendar month page element*/}
-    </Route>
-    <Route path="*" element={<div>Page not found...</div>} />
-  </Routes>
-</>;
