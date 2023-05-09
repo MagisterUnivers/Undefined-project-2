21b55a0169b56ac393bc3b28eba539986989f455
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-09 14:25:35 +0300

    AS-Fix

--- src/components/Forms/UserForm/UserForm.jsx
+++ src/components/Forms/UserForm/UserForm.jsx
@@ -297,22 +297,22 @@ const StyledForm = styled.form`
   /* gap: 20px; */
 `;
 
-const StyledTitle = styled.h2`
-  text-align: center;
-  margin-top: 40px;
-  margin-bottom: 32px;
+// const StyledTitle = styled.h2`
+//   text-align: center;
+//   margin-top: 40px;
+//   margin-bottom: 32px;
 
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: calc(24 / 18);
+//   font-family: 'Inter';
+//   font-style: normal;
+//   font-weight: 600;
+//   font-size: 18px;
+//   line-height: calc(24 / 18);
 
-  color: #3e85f3;
+//   color: #3e85f3;
 
-  text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
-    0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
-`;
+//   text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
+//     0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
+// `;
 
 const StyledLabel = styled.label`
   font-family: 'Inter';
