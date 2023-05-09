9e481db2e23d7ba8b396497ec4a2be577ff5b319
Dan Dudaiev <roffrooff@gmail.com>
2023-05-09 11:36:38 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,18 @@ import LoginPage from 'pages/LoginPage/LoginPage';
 export const App = () => {
   return (
     <>
-      <MainLayout />
       <Routes>
-        <Route index /> {/*TODO: About page element*/}
+        <Route path="/about" /> {/*TODO: About page element*/}
         <Route path="/register" /> {/*TODO: Register page element*/}
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/calendar" /> {/*TODO: Calendar page element*/}
-        <Route path="/calendar/day/:currentDay" />{' '}
-        {/*TODO: Calendar day page element*/}
-        <Route path="/calendar/month/:currentDate" />{' '}
-        {/*TODO: Calendar month page element*/}
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/calendar" /> {/*TODO: Calendar page element*/}
+          <Route path="/calendar/day/:currentDay" />{' '}
+          {/*TODO: Calendar day page element*/}
+          <Route path="/calendar/month/:currentDate" />{' '}
+          {/*TODO: Calendar month page element*/}
+          <Route path="*" element={<div>Page not found...</div>} />
+        </Route>
       </Routes>
     </>
   );
