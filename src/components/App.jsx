0811f2bc437d7dca67ec5df9948dc262f33ae9d4
Irgey <116591589+Irgey@users.noreply.github.com>
2023-05-09 20:28:58 +0200

    header desktop version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,27 +5,25 @@ import LoginPage from 'pages/LoginPage/LoginPage';
 import RegisterPage from 'pages/RegisterPage/RegisterPage';
 import { UserForm } from './Forms/UserForm/UserForm';
 import AccountPage from 'pages/AccountPage/AccountPage';
-// import { Header } from './Header/Header';
+import { Header } from './Header/Header';
 
 export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/about" /> {/*TODO: About page element*/}
-        <Route path="/register" element={<RegisterPage />} />{' '}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/test" element={<UserForm />} />
-        <Route path="/" element={<MainLayout />}>
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/calendar" /> {/*TODO: Calendar page element*/}
-          <Route path="/calendar/day/:currentDay" />{' '}
-          {/*TODO: Calendar day page element*/}
-          <Route path="/calendar/month/:currentDate" />{' '}
-          {/*TODO: Calendar month page element*/}
-        </Route>
-        <Route path="*" element={<div>Page not found...</div>} />
-      </Routes>
-    </>
-    // <Header />
-  );
+  return <Header />;
 };
+<>
+  <Routes>
+    <Route path="/about" /> {/*TODO: About page element*/}
+    <Route path="/register" element={<RegisterPage />} />{' '}
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/test" element={<UserForm />} />
+    <Route path="/" element={<MainLayout />}>
+      <Route path="/account" element={<AccountPage />} />
+      <Route path="/calendar" /> {/*TODO: Calendar page element*/}
+      <Route path="/calendar/day/:currentDay" />{' '}
+      {/*TODO: Calendar day page element*/}
+      <Route path="/calendar/month/:currentDate" />{' '}
+      {/*TODO: Calendar month page element*/}
+    </Route>
+    <Route path="*" element={<div>Page not found...</div>} />
+  </Routes>
+</>;
