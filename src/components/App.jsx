2eb318d17b770dc33d83a7e44d5b06f9cd110fa6
Dan Dudaiev <roffrooff@gmail.com>
2023-05-09 11:24:01 +0300

    basic routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
 // import { RegisterForm } from './Forms/RegisterForm/RegisterForm';
+import { Route, Routes } from 'react-router';
 import LogOut from './Btn/LogoutBtn/LogOut';
 import { LoginForm } from './Forms/LoginForm/LoginForm';
 import { MainLayout } from './MainLayout/MainLayout';
+import LoginPage from 'pages/LoginPage/LoginPage';
 
 export const App = () => {
   return (
-    <div>
-      <LoginForm />
-      <LogOut />
+    <>
       <MainLayout />
-    </div>
+      <Routes>
+        <Route index /> {/*TODO: About page element*/}
+        <Route path="/register" /> {/*TODO: Register page element*/}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/calendar" /> {/*TODO: Calendar page element*/}
+        <Route path="/calendar/day/:currentDay" />{' '}
+        {/*TODO: Calendar day page element*/}
+        <Route path="/calendar/month/:currentDate" />{' '}
+        {/*TODO: Calendar month page element*/}
+      </Routes>
+    </>
   );
 };
