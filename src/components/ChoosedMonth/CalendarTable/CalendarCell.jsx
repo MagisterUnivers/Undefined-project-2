da8efe89c3ef949a7ea9e6d679c3a41eb94e66d9
juliakarnaukh <juliakarnayh04@gmail.com>
2023-05-12 17:00:23 +0300

    fist

--- src/components/ChoosedMonth/CalendarTable/CalendarCell.jsx
+++ src/components/ChoosedMonth/CalendarTable/CalendarCell.jsx
@@ -5,6 +5,8 @@ import { useEventTasks } from '../../../redux';
 const LOW = 'low';
 const HIGHT = 'hight';
 const MEDIUM = 'medium';
+import { useNavigate } from 'react-router-dom';
+
 /**
  * Cell of the month calendar
  */
@@ -23,7 +25,8 @@ export const CalendarCell = ({ state, date }) => {
     formattedDate,
   } = useCalendarCell({ date }, state, ref);
 
-  // _id: '64303c8582dc6fccdee4f8d2',
+  //     task:
+  //     _id: '64303c8582dc6fccdee4f8d2',
   //     title: 'toDo',
   //     start: '9-00',
   //     end: '14-00',
@@ -34,6 +37,11 @@ export const CalendarCell = ({ state, date }) => {
   //     createdAt: '2023-04-07T15:53:41.088Z',
   //     updatedAt: '2023-04-07T15:53:41.088Z',
   //     __v: 0,[]
+
+  const navigate = useNavigate();
+  const handelClick = () => {
+    navigate(`calendar/day/${formattedDate}`);
+  };
 
   return (
     <td
@@ -48,6 +56,7 @@ export const CalendarCell = ({ state, date }) => {
           isOutsideVisibleRange ? 'text-gray-200 ' : ''
         }`}
         {...buttonProps}
+        onClick={handelClick}
       >
         <span
           className={`font-inter font-bold text-xs 
