9089787707d93d0e6098db6c1b753ab5421feee7
Dan Dudaiev <roffrooff@gmail.com>
2023-05-12 22:26:47 +0300

    Login trash delete

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,24 +1,13 @@
 import { LoginForm } from 'components/Forms/LoginForm/LoginForm';
 import { StyledIcon, StyledIconWrapper } from './StyledLoginPage';
-import Modal from 'components/Modal/Modal';
-import { useState } from 'react';
-import TaskForm from 'components/TaskForm/TaskForm';
 
 const LoginPage = () => {
-  const [isModalShown, setIsModalShown] = useState(true);
   const wrapper = document.querySelector('body');
   wrapper.style.backgroundColor = '#DCEBF7';
 
-  const handleModalClose = () => {
-    setIsModalShown(false);
-  };
-
   return (
     <>
-      {isModalShown && (
-        <Modal handleModalClose={handleModalClose}>{<TaskForm />}</Modal>
-      )}
-
+      <LoginForm />
       <StyledIconWrapper>
         <StyledIcon />
       </StyledIconWrapper>
